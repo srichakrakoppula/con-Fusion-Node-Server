8f8f4fde6b7723a08059733747ff8d7a935e60db
srichakra <srichakra.koppula@gmail.com>
2018-03-14 12:04:30 -0400

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -47,22 +49,21 @@ app.use(session({
 }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
 function auth(req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
       return next(err);
-  }else if(req.session.user==='authenticated'){
+  }else {
     next();
-  }else{
-    var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
   }
 }
 
